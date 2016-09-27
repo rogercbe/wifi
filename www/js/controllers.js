5bf944749038997d617a7a81c3958e98612a994b
Roger Cendrós <rogercbe@gmail.com>
2016-09-27 21:01:17 +0200

    init

---
+++ www/js/controllers.js
@@ -0,0 +1,48 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+  $scope.scan = function () {
+    WifiWizard.getCurrentSSID(ssidHandler, fail);
+    WifiWizard.getCurrentBSSID(bssidHandler, fail);
+  }
+
+  function ssidHandler(data) {
+    console.log(data);
+    $scope.ssid = data;
+  }
+
+  function bssidHandler(data) {
+    console.log(data);
+    $scope.bssid = data;
+  }
+
+  function fail(error) {
+    console.log(error);
+    $scope.data = error;
+  }
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
